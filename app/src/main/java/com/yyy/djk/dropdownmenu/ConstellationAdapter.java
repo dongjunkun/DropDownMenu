4d99c21028930e01c4c93d2e8de402beef2c3704
dongjunkun <1440418246@qq.com>
2015-07-11 10:42:33 +0800

    add constellation adapter

---
+++ app/src/main/java/com/yyy/djk/dropdownmenu/ConstellationAdapter.java
@@ -0,0 +1,84 @@
+package com.yyy.djk.dropdownmenu;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.yyy.djk.multipledropdownmenu.R;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+
+public class ConstellationAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<String> list;
+    private int checkItemPosition = 0;
+
+    public void setCheckItem(int position) {
+        checkItemPosition = position;
+        notifyDataSetChanged();
+    }
+
+    public ConstellationAdapter(Context context, List<String> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView != null) {
+            viewHolder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_constellation_layout, null);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
+        fillValue(position, viewHolder);
+        return convertView;
+    }
+
+    private void fillValue(int position, ViewHolder viewHolder) {
+        viewHolder.mText.setText(list.get(position));
+        if (checkItemPosition != -1) {
+            if (checkItemPosition == position) {
+                viewHolder.mText.setTextColor(context.getResources().getColor(R.color.drop_down_selected));
+                viewHolder.mText.setBackgroundResource(R.drawable.check_bg);
+            } else {
+                viewHolder.mText.setTextColor(context.getResources().getColor(R.color.drop_down_unselected));
+                viewHolder.mText.setBackgroundResource(R.drawable.uncheck_bg);
+            }
+        }
+    }
+
+    static class ViewHolder {
+        @InjectView(R.id.text)
+        TextView mText;
+
+        ViewHolder(View view) {
+            ButterKnife.inject(this, view);
+        }
+    }
+}
